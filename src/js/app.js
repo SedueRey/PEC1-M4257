20d2f08d9ea9016a3c90ca4eff82b40afcabd2c7
Eduardo - Sedue - Rey <proyectos@seduerey.com>
2021-03-29 18:44:23 +0200

    configurado para el despliegue

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.toggle('js_menuopened');
   });
   document.getElementById('menubg').addEventListener('click', (e) => {
-    e.preventDefault();
     if(e.target.id === 'menubg') {
       document.body.classList.remove('js_menuopened');
     }
