ad724ee7ce45180df1d7cc7b191e0c9751e159cf
Eduardo - Sedue - Rey <proyectos@seduerey.com>
2021-05-08 17:12:15 +0200

    Portada

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PEC1-M4257
 PEC1. Desarrollo de una web
+PEC 2: web y recursos multimedia
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
+function debounce(func, wait = 100) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('hamburger').addEventListener('click', () => {
     document.body.classList.toggle('js_menuopened');
@@ -7,4 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
       document.body.classList.remove('js_menuopened');
     }
   });
+  window.addEventListener('scroll', 
+    debounce(() => {
+      console.log('accede');
+      const headerHeight = document.querySelector('.mainheader').offsetHeight;
+      if(window.scrollY > headerHeight) {
+        if (!document.body.classList.contains('js_fixedheader')) {
+          document.body.classList.add('js_fixedheader');
+        }
+      } else {
+        document.body.classList.remove('js_fixedheader');
+      }
+    })
+  );
 });
